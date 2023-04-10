2748fbcac9fce50de57bcbfb8a38f32baeac300f
seenubhai123 <thattesreenivasulu7876@gmail.com>
2023-04-10 07:49:34 +0000

    ipl dashboard in react js

---
+++ src/components/TeamCard/index.js
@@ -0,0 +1,22 @@
+// Write your code here
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import './index.css'
+
+export default class TeamCard extends Component {
+  render() {
+    const {teamCard} = this.props
+    const {id, name, teamImageUrl} = teamCard
+    console.log(name)
+    return (
+      <li className="links">
+        <div>
+          <img className="teamLogo" src={teamImageUrl} alt={name} />
+        </div>
+        <div className="heading">
+          <p>{name}</p>
+        </div>
+      </li>
+    )
+  }
+}
